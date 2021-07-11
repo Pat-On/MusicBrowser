d4a6effe77dd346ea52124d8e46f9b5baf4959e8
Patryk <patryk.r.nowak@gmail.com>
2021-07-11 22:39:29 +0100

    inheritance in tkinter as a extention of the Listbox by scrollbar functionality built in

--- jukebox.py
+++ jukebox.py
@@ -4,8 +4,24 @@ try:
     import tkinter
 except ImportError:  # Python2
     import Tkinter as tkinter
-
+# sql connection
 conn = sqlite3.connect('music.sqlite')
+
+# subclasses base on the Listbox with added already scrollbar !INHERITANCE
+class Scrollbox(tkinter.Listbox):
+    # init method
+    def __init__(self, window, **kwargs):
+        # kinter.Listbox.__init__(self, window, **kwargs) # Python 2 version
+        super().__init__(window, **kwargs)
+
+        self.scrollbar = tkinter.Scrollbar(window, orient=tkinter.VERTICAL, comman=self.yview)
+
+    def grid(self, row, column, sticky='nsw', rowspan=1, columnspan=1, **kwargs):
+        # tkinter.Listbox.grid(self, row=row, column=column, sticky=sticky, rowspan=rowspan, **kwargs) #python2
+        super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, **kwargs)  # calling supper method
+        self.scrollbar.grid(row=row, column=column, sticky='nse', rowspan=rowspan)
+        self['yscrollcommand'] = self.scrollbar.set
+
 
 mainWindow = tkinter.Tk()
 mainWindow.title('Music DB Browser')
@@ -28,14 +44,14 @@ tkinter.Label(mainWindow, text="Songs").grid(row=0, column=2)
 
 # ARTISTS LISTBOX
 
-artistList = tkinter.Listbox(mainWindow)
+artistList = Scrollbox(mainWindow, background="lightblue")  # it can pass additional because of **kwargs
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
 
 # we are telling it to use yview opposite is xview
-artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
-artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
-artistList['yscrollcommand'] = artistScroll.set  # it will notify list box that to set method if anything
+# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
+# artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
+# artistList['yscrollcommand'] = artistScroll.set  # it will notify list box that to set method if anything
 # is going to happen what should change the scroll like arrows  -> way of implement it in any widget what support it!
 
 # ALBUM LISTBOX
@@ -43,19 +59,21 @@ artistList['yscrollcommand'] = artistScroll.set  # it will notify list box that 
 albumLV = tkinter.Variable(mainWindow)
 # tkinter variable in python are tracked so if anything is going to change the python is going to be notify
 albumLV.set(("Choose an artist",))  # tuple
-albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)  # is variable is going to change it is going to be notify
+# albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)  # is variable is going to change it is going to be notify
+albumList = Scrollbox(mainWindow, listvariable=albumLV)  # is variable is going to change it is going to be notify
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
 
-albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
-albumScroll.grid(row=1, column=1, sticky='nse', rowspan=2)
-albumList['yscrollcommand'] = albumScroll.set
+# albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
+# albumScroll.grid(row=1, column=1, sticky='nse', rowspan=2)
+# albumList['yscrollcommand'] = albumScroll.set
 
 # SONGS LISTBOX
 
 songLV = tkinter.Variable(mainWindow)
 songLV.set(("Choose an album",))  # coma because again it is tuple
-songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+# songList = tkinter.Listbox(mainWindow, listvariable=songLV)
+songList = Scrollbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
