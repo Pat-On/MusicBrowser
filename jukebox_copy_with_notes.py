27167f9a8a2ec7a86265545e230622a43a0c0653
Patryk <patryk.r.nowak@gmail.com>
2021-07-11 23:00:15 +0100

    connected db to the tkinter + added even <<ListboxSelect>> to trigger event

--- jukebox_copy_with_notes.py
+++ jukebox_copy_with_notes.py
@@ -7,7 +7,6 @@ except ImportError:  # Python2
 # sql connection
 conn = sqlite3.connect('music.sqlite')
 
-
 # subclasses base on the Listbox with added already scrollbar !INHERITANCE
 class Scrollbox(tkinter.Listbox):
     # init method
@@ -22,20 +21,6 @@ class Scrollbox(tkinter.Listbox):
         super().grid(row=row, column=column, sticky=sticky, rowspan=rowspan, **kwargs)  # calling supper method
         self.scrollbar.grid(row=row, column=column, sticky='nse', rowspan=rowspan)
         self['yscrollcommand'] = self.scrollbar.set
-
-
-# e.target in JS
-def get_albums(event):
-    lb = event.widget
-    index = lb.curselection()[0]
-    artist_name = lb.get(index),  # this coma at the end is going to change it to tuple
-
-    # get the artist ID from the database row
-    artist_id = conn.execute("SELECT artists._id FROM artists WHERE artists.name=?", artist_name).fetchone()
-    alist = []
-    for row in conn.execute("SELECT albums.name FROM albums WHERE albums.artist=? ORDER BY albums.name", artist_id):
-        alist.append(row[0])
-    albumLV.set(tuple(alist))
 
 
 mainWindow = tkinter.Tk()
@@ -63,32 +48,40 @@ artistList = Scrollbox(mainWindow, background="lightblue")  # it can pass additi
 artistList.grid(row=1, column=0, sticky='nsew', rowspan=2, padx=(30, 0))
 artistList.config(border=2, relief='sunken')
 
-# population of the first Listbox
-for artist in conn.execute("SELECT artists.name FROM artists ORDER BY artists.name"):
-    # print(artist)
-    artistList.insert(tkinter.END, artist[0])
-
-# We can bind our function to virual events what may happen on the listbox
-artistList.bind('<<ListboxSelect>>', get_albums)  # case sensitive if ListBox... it not working
+# we are telling it to use yview opposite is xview
+# artistScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=artistList.yview)
+# artistScroll.grid(row=1, column=0, sticky='nse', rowspan=2)
+# artistList['yscrollcommand'] = artistScroll.set  # it will notify list box that to set method if anything
+# is going to happen what should change the scroll like arrows  -> way of implement it in any widget what support it!
 
 # ALBUM LISTBOX
+
 albumLV = tkinter.Variable(mainWindow)
+# tkinter variable in python are tracked so if anything is going to change the python is going to be notify
 albumLV.set(("Choose an artist",))  # tuple
+# albumList = tkinter.Listbox(mainWindow, listvariable=albumLV)  # is variable is going to change it is going to be notify
 albumList = Scrollbox(mainWindow, listvariable=albumLV)  # is variable is going to change it is going to be notify
 albumList.grid(row=1, column=1, sticky='nsew', padx=(30, 0))
 albumList.config(border=2, relief='sunken')
+
+# albumScroll = tkinter.Scrollbar(mainWindow, orient=tkinter.VERTICAL, command=albumList.yview)
+# albumScroll.grid(row=1, column=1, sticky='nse', rowspan=2)
+# albumList['yscrollcommand'] = albumScroll.set
 
 # SONGS LISTBOX
 
 songLV = tkinter.Variable(mainWindow)
 songLV.set(("Choose an album",))  # coma because again it is tuple
+# songList = tkinter.Listbox(mainWindow, listvariable=songLV)
 songList = Scrollbox(mainWindow, listvariable=songLV)
 songList.grid(row=1, column=2, sticky='nsew', padx=(30, 0))
 songList.config(border=2, relief='sunken')
 
 # MAIN LOOP
-# testList = range(0, 100)
-# albumLV.set(tuple(testList))
+testList = range(0, 100)
+albumLV.set(tuple(testList))
+# albumLV.set((1, 2, 3, 4, 5)) # You have to use set You can not assign them straight forward
 mainWindow.mainloop()
 print("closing db connection")
 conn.close()
+
